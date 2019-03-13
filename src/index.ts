195961bc5fb8e7090a031f42dcc4638fa3cf3064
Evans Owino <evansowino@hotmail.com>
2019-03-13 22:29:37 +0000

    feat: basic functionality of pwnd

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,67 @@
 import axios from "axios";
 import sha1 from "sha1";
 
-const URL = "https://api.pwnedpasswords.com/range/";
+interface IResponse {
+  data: string;
+}
 
-async function fetchPasswordRange(hashSubstr: string) {
-  const res = await axios.get(`${URL}${hashSubstr}`);
-  const data: string = res.data;
-  return data.split("\r\n");
+const URL = "https://api.pwnedpasswords.com/range/";
+const NO_PASSWORDS_PROVIDED = "Please provide a list of passwords to check";
+let passwords: string[] = [];
+let hashedPasswords: string[] = [];
+
+function fetchPasswordRange(hashSubstr: string) {
+  return axios.get(`${URL}${hashSubstr}`);
+}
+
+function parseArgs() {
+  passwords = process.argv.slice(2);
+  if (!passwords.length) {
+    console.log(NO_PASSWORDS_PROVIDED);
+    process.exit();
+  }
+}
+
+function getParsedResponse(response: IResponse): string[] {
+  try {
+    const data = response.data;
+    return data.split("\r\n");
+  } catch {
+    return [];
+  }
+}
+
+function findMatch(hashedPassword: string, possibleHashes: string[]) {
+  return possibleHashes.find(possibleHash => {
+    const [hash] = possibleHash.split(":");
+    return hashedPassword.includes(hash);
+  });
+}
+
+function logResult(password: string, foundHash: string) {
+  if (foundHash) {
+    const [hash, count] = foundHash.split(":");
+    console.log(`The following password was found: ${password}`);
+    console.log(`Hash: ${hash}, Occurence: ${count}\n`);
+  } else {
+    console.log(`The following password was not found: ${password}`);
+  }
 }
 
 async function run() {
-  const passwords: string[] = process.argv.slice(2);
-  const hashes: string[] = passwords.map(password =>
-    (sha1(password) as string).substr(0, 5)
+  parseArgs();
+  hashedPasswords = passwords.map(password =>
+    (sha1(password) as string).toUpperCase()
   );
+  const fetchResults = hashedPasswords.map(pwd =>
+    fetchPasswordRange(pwd.substring(0, 5))
+  );
+  const responses = await Promise.all(fetchResults);
+  const possibleHashes = responses.map(response => getParsedResponse(response));
 
-  for (const hash of hashes) {
-    const res = await fetchPasswordRange(hash);
-    console.log(res);
+  for (let i = 0; i < hashedPasswords.length; i++) {
+    const match = findMatch(hashedPasswords[i], possibleHashes[i]);
+    logResult(passwords[i], match);
   }
 }
 
